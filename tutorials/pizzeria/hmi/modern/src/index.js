f39bbf2127fa9d139286885207741651d9525788
SamyOffer <samy.offer@student.vinci.be>
2022-10-04 11:22:53 +0200

    modern-hmi

--- tutorials/pizzeria/hmi/modern/src/index.js
+++ tutorials/pizzeria/hmi/modern/src/index.js
@@ -1,2 +1,18 @@
+// Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Custom styles
 import './stylesheets/main.css';
+// Import Animte.css
+import 'animate.css';
+
+
+const body = document.querySelector('body');
+
+body.addEventListener('click', startOrStopSound);
+
+function startOrStopSound() {
+  const myAudioPlayer = document.querySelector('#audioPlayer');
+
+  if (myAudioPlayer.paused) myAudioPlayer.play();
+  else myAudioPlayer.pause();
+}
