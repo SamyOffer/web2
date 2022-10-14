3e48324482e11e9c3447bff24eca6220b2941b24
SamyOffer <samy.offer@student.vinci.be>
2022-10-14 11:43:19 +0200

    fix5

---
+++ tutorials/exercices/module2/2.1/js-basic-boilerplate/src/index.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+/** 
+import popSmoke from './img/p.jpg';
+
+const footerPhoto = new Image(); // or document.createElement('img');
+footerPhoto.src = popSmoke;
+footerPhoto.height = 400;
+const footer = document.querySelector("main");
+footer.appendChild(footerPhoto);
+
+*/
